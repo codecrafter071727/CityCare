d4fc9cfb8af96888d0d89cf59c0c1f47fde951da
vishu pratap <vishurizz01@gmail.com>
2024-08-24 19:34:25 +0530

    signup route implemented

--- Server_side/routes/hospital.js
+++ Server_side/routes/hospital.js
@@ -1,9 +1,58 @@
-const express = require('express')
+const express = require("express");
+const zod = require("zod");
+const zwt = require("jsonwebtoken");
+const { Hospital } = require("../db");
+const { JWT_SECRET } = require("../configuration");
+const router = express.Router();
 
-const router = express.Router;
+const signupBody = zod.object({
+  hospitalName: zod.string(),
+  email: zod.string().email(),
+  password: zod.string().min(3),
+  contactNumber: zod.string(),
+  address: zod.string(),
+  numberOfBeds: zod.number(),
+});
 
-router.post('/hospitalsignup', (req, res)=>{
-    const {hospitalName, email, password, contactNumber, address, numberOfBeds} = req.body;
-    
-}
-)
+router.post("/signup", async (req, res) => {
+  const {
+    hospitalName,
+    email,
+    password,
+    contactNumber,
+    address,
+    numberOfBeds,
+  } = req.body;
+  const { success } = signupBody.safeParse(req.body);
+  if (!success) {
+    return res.status(411).json({
+      message: "Email already taken / Incorrect inputs",
+    });
+  }
+
+  const existingUser = await Hospital.findOne({
+    hospitalName: hospitalName,
+  });
+
+  if (existingUser) {
+    return res.status(411).json({
+      message: "Email already taken/Incorrect inputs",
+    });
+  }
+  const hospital = await Hospital.create({
+    hospitalName,
+    email,
+    password,
+    contactNumber,
+    address,
+    numberOfBeds,
+  });
+  const token = zwt.sign({ hospitalId: hospital._id }, JWT_SECRET);
+
+  res.status(200).json({
+    message: "Hospital registered successfully",
+    token,
+  });
+});
+
+module.exports = router;
--- Server_side/routes/index.js
+++ Server_side/routes/index.js
@@ -2,10 +2,12 @@
 const express = require('express');
 const userRouter = require("./user");
 // const accountRouter = require("./account");
-
+const hospitalRouter = require('./hospital')
 const router = express.Router();
 
 router.use("/user", userRouter);
+router.use("/hospital", hospitalRouter)
 // router.use("/account", accountRouter);
+
 
 module.exports = router;
